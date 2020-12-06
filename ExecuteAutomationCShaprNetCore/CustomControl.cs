8dd6c9c812988758d1a77fe41dfcda5c0d1933ca
mitvan91 <ivanmit1809@gmail.com>
2020-12-06 19:29:43 +0100

    initial Commit until combo controls
    initial Commit until combo controls

---
+++ ExecuteAutomationCShaprNetCore/CustomControl.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExecuteAutomationCShaprNetCore
+{
+    public class CustomControl : DriverHelper
+    {
+        public static void ComboBox(String controlName, string value)
+        {
+            IWebElement comboControl = Driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+            comboControl.Clear();
+            comboControl.SendKeys(value);
+            Driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{value}']")).Click();
+        }
+    }
+}
---
+++ ExecuteAutomationCShaprNetCore/DriverHelper.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExecuteAutomationCShaprNetCore
+{
+    public class DriverHelper
+    {
+        public static IWebDriver Driver { get; set; }
+       
+    }
+}
